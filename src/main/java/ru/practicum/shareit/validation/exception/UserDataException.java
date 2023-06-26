53fda76cd47c674c4c934a1d861fea63fe3ff8d4
Дмитрий <dkorokhov@yandex.ru>
2023-06-26 16:49:42 +0300

    1 сдача ТЗ 13

--- src/main/java/ru/practicum/shareit/validation/exception/UserDataException.java
+++ src/main/java/ru/practicum/shareit/validation/exception/UserDataException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.validation.exception;
 
-public class UserDataException extends RuntimeException{
+public class UserDataException extends RuntimeException {
     public UserDataException(String message) {
         super(message);
     }
