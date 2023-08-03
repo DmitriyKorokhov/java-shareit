f93c435f0ecdca7e453fdad0c6ff07d85267f38f
DmitriyKorokhov <116824479+DmitriyKorokhov@users.noreply.github.com>
2023-08-03 16:35:44 +0300

    Update ErrorHandler.java

--- src/main/java/ru/practicum/shareit/validation/errorhandler/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/validation/errorhandler/ErrorHandler.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.validation.errorhandler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
