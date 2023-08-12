348732a8c35bb067c58d74aa4e1e591bb7202300
DmitriyKorokhov <116824479+DmitriyKorokhov@users.noreply.github.com>
2023-08-12 13:36:25 +0300

    Delete src directory

--- src/main/java/ru/practicum/shareit/validation/errorhandler/ErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.validation.errorhandler;
-
-import lombok.Data;
-
-@Data
-public class ErrorResponse {
-    private final String error;
-    private final String description;
-}
--- src/main/java/ru/practicum/shareit/validation/exception/IncorrectStatusException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.shareit.validation.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class IncorrectStatusException extends ValidationException {
-    public IncorrectStatusException(HttpStatus status, String message) {
-        super(status, message);
-    }
-}
